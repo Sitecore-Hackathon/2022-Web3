7ddd3af31038245d904d5c3e0cd8d23dc82f09dc
Per Manniche Bering <pmbering@fastmail.com>
2022-03-05 11:28:08 +0100

    chore(cm): added single instance cm

---
+++ glitterfish/docker/build/cm/GlitterfishBoot.ps1
@@ -0,0 +1,56 @@
+$cleanDatabasesPath = "C:\clean_databases"
+$sqlLogin = "IIS APPPOOL\DefaultAppPool"
+$sqlDataRoot = "C:\mssql"
+$sqlServer = ".\SQLEXPRESS"
+$sqlServiceName = "MSSQL$`SQLEXPRESS"
+$sqlLoginTimeout = 30
+
+# ensure SQL is running
+Write-Host "### Waiting on SQL service '$sqlServiceName'."
+
+(Get-Service $sqlServiceName ).WaitForStatus("Running")
+
+Write-Host "### SQL service '$sqlServiceName' ready."
+
+# ensure databases ready
+if ((Test-Path $sqlDataRoot) -eq $false)
+{
+    New-Item -Path $sqlDataRoot -ItemType Directory | Out-Null
+}
+
+$noSitecoreDatabases = $null -eq (Get-ChildItem -Path $sqlDataRoot -Filter "Sitecore*.mdf")
+
+if ($noSitecoreDatabases)
+{
+    Write-Host "### No Sitecore databases found in '$sqlDataRoot', seeding clean databases from '$cleanDatabasesPath'."
+
+    Get-ChildItem -Path $cleanDatabasesPath | Copy-Item -Destination $sqlDataRoot
+}
+
+Get-ChildItem -Path $sqlDataRoot -Filter "*.mdf" | ForEach-Object {
+    $databaseName = $_.BaseName.Replace("_Primary", "")
+    $mdfPath = $_.FullName
+    $ldfPath = $mdfPath.Replace(".mdf", ".ldf")
+    $sqlcmd = "IF EXISTS (SELECT 1 FROM SYS.DATABASES WHERE NAME = '$databaseName') BEGIN EXEC sp_detach_db [$databaseName] END;CREATE DATABASE [$databaseName] ON (FILENAME = N'$mdfPath'), (FILENAME = N'$ldfPath') FOR ATTACH;"
+
+    Write-Host "### Attaching '$databaseName'..."
+
+    sqlcmd.exe -S $sqlServer -Q $sqlcmd -l $sqlLoginTimeout
+}
+
+Write-Host "### Databases ready."
+
+# ensure SQL credentials ready
+$deploySqlLogin = (sqlcmd.exe -S $sqlServer -Q "SELECT COUNT(*) FROM sys.server_principals WHERE name = '$sqlLogin';" -h -1 -W -l $sqlLoginTimeout | Select-Object -First 1) -eq "0"
+
+if ($deploySqlLogin)
+{
+    sqlcmd.exe -S $sqlServer -Q "CREATE LOGIN [$sqlLogin] FROM Windows; EXEC sp_addsrvrolemember '$sqlLogin', sysadmin;" -l $sqlLoginTimeout
+}
+
+Write-Host "### SQL credentials ready."
+
+# ensure Sitecore admin password
+. (Join-Path $PSScriptRoot "\SetSitecoreAdminPassword.ps1") -ResourcesDirectory $PSScriptRoot -SitecoreAdminPassword $env:SITECORE_ADMIN_PASSWORD -SqlServer $sqlServer
+
+Write-Host "### Sitecore admin credentials ready."
