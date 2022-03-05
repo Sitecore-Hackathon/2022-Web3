dd0696ddee6e285909a8ad60a32c0b3f73fc916f
Jesper Balle <Jesper.Balle@valtech.com>
2022-03-05 21:20:36 +0100

    add logs command

---
+++ Operator/src/sitecore-cli/Commands/LogsCommand.cs
@@ -0,0 +1,21 @@
+﻿using Sitecore.Devex.Client.Cli.Extensibility.Subcommands;
+using System;
+using System.Threading.Tasks;
+using Web3.Operator.Cli.Tasks;
+
+namespace Web3.Operator.Cli.Commands
+{
+    public class LogsCommand : SubcommandBase<LogsTask, LogsArgs>
+    {
+        public LogsCommand(IServiceProvider container) : base("logs", "Show logs from instance", container)
+        {
+            AddOption(ArgOptions.InstanceName);
+        }
+
+        protected override async Task<int> Handle(LogsTask task, LogsArgs args)
+        {
+            await task.Execute(args).ConfigureAwait(false);
+            return 0;
+        }
+    }
+}
