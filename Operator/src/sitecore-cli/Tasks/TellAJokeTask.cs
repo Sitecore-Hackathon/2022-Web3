a4ea4d3425f756e01de5c5fb5467e43b0829307e
Steven Aneel Hasz-Singh <steven@apply.agency>
2022-03-05 15:59:52 +0100

    Fancy jokes

--- Operator/src/sitecore-cli/Tasks/TellAJokeTask.cs
+++ Operator/src/sitecore-cli/Tasks/TellAJokeTask.cs
@@ -3,6 +3,8 @@ using DadJokeCli.Commands;
 using Microsoft.Extensions.Logging;
 using Sitecore.DevEx.Client.Logging;
 using System;
+using System.Collections.Generic;
+using System.Linq;
 using System.Threading.Tasks;
 
 namespace DadJokeCli.Tasks
@@ -10,6 +12,15 @@ namespace DadJokeCli.Tasks
     public class TellAJokeTask
     {
         private ILogger<TellAJokeTask> _logger;
+        private Dictionary<string, string> sitecoreJokes = new Dictionary<string, string>
+        {
+            {"I have a joke about why are Sitecore Developers are so forgetful? Unfortunately I don't remember it because Sitecore is using all my memory.", "@Steven Singh" },
+            {"I have a joke about Sitecore, but I'm still waiting for it to load.", "@jammykam" },
+            {"I have a Sitecore joke, but it’s not been published yet.", "@MobeenAnwar" },
+            {"I have a joke about sitecore, but it’s not in my version. And it’s 30k in license cost to upgrade. :)", "@ChristopherAuer" },
+            {"I'd send you a joke about EXM but you might not get it.", "@handee_andee" },
+            {"I have a joke about being an MVP, but it's under NDA so I can't tell anyone... ", "@kenkre_rohan" },
+        };
 
         public TellAJokeTask(ILogger<TellAJokeTask> logger)
         {
@@ -19,18 +30,22 @@ namespace DadJokeCli.Tasks
         {
             if (args.SitecoreJoke)
             {
-                //I have a joke about Sitecore, but I'm still waiting for it to load. @jammykam
-                //I have a Sitecore joke, but it’s not been published yet. @MobeenAnwar
-                //I have a joke about sitecore, but it’s not in my version. And it’s 30k in license cost to upgrade. :) @ChristopherAuer
-                //I'd send you a joke about EXM but you might not get it. @handee_andee
-                //I have a joke about being an MVP, but it's under NDA so I can't tell anyone... @kenkre_rohan
-                ColorLogExtensions.LogConsole(_logger, LogLevel.Error, "I have a joke about why are Sitecore Developers are so forgetful? Unfortunately I don't remember it because Sitecore is using all my memory");
+                Random random = new Random();
+                var jokeIndex = random.Next(sitecoreJokes.Count);
+
+                var randomJoke = sitecoreJokes.ElementAt(jokeIndex);
+                ColorLogExtensions.LogConsole(_logger, LogLevel.Error, GetJokeText(randomJoke.Key, randomJoke.Value));
                 return;
             }
 
             ICanHazDadJokeClient client = new ICanHazDadJokeClient("FxxK PxTxI!");
             var joke = await client.GetRandomJokeAsync();
-            ColorLogExtensions.LogConsole(_logger, LogLevel.Warning, joke.DadJoke);
+            ColorLogExtensions.LogConsole(_logger, LogLevel.Warning, GetJokeText(joke.DadJoke, "ICanHazDadJoke"));
+        }
+
+        private string GetJokeText(string joke, string author)
+        {
+            return $"{joke} by {author}";
         }
     }
 }
